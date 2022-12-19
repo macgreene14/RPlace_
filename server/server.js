b2f96c94c1638d08a9acc43c55b9c9aab9e48b2e
jmg <macgreene14@gmail.com>
2022-12-19 16:33:31 -0500

    attempt 5

--- server/server.js
+++ server/server.js
@@ -19,10 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
-  });
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
 app.get("/", (req, res) => {
